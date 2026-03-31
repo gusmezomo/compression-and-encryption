277067782460439662566117f7fae1b7e83c0490
Eduardo Cansan <eduardocansann@gmail.com>
2026-03-31 19:59:44 -0300

    feat: add almost ended encode fibonacci

--- logic/fibonacci.py
+++ logic/fibonacci.py
@@ -1,34 +1,3 @@
-# # Transform char into decimal in ASCII table
-# def text_to_decimals(text: str) -> list[int]:
-#     return [ord(char) for char in text]
-
-# text = input("Choose something: ")
-# decimal_text = text_to_decimals(text)
-# print(decimal_text)
-
-# fibonacci_reverse = [233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 2, 1]
-# print(fibonacci_reverse)
-
-# # fibonacci = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
-# # print(fibonacci)
-
-# encode = []
-
-# for char in decimal_text:
-#     for fibo_char in fibonacci_reverse:
-#         if char <= fibo_char:
-#             encode.append(0')
-#         else:
-#             aux = char
-#             print(aux)
-#             encode.append(1)
-#             char = char - fibo_char
-            
-# print(encode)
-            
-# real_encode = encode.reverse()
-# print(real_encode)
-
 # transform char into decimal in ASCII table
 def text_to_decimals(text: str) -> list[int]:
     return [ord(char) for char in text]
@@ -38,6 +7,7 @@ def fibonacci_encode(decimal: int) -> list[int]:
     fibonacci_reverse = [233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 2, 1]
     bits = []
 
+    # the logic to put 1 and 0 in the correct places
     for fibo in fibonacci_reverse:
         if decimal >= fibo:
             bits.append(1)
@@ -46,19 +16,21 @@ def fibonacci_encode(decimal: int) -> list[int]:
             bits.append(0)
 
     bits.reverse()  # reverse in place, then return separately
-    return bits
-    
-    # return ''.join(str(bit) for bit in bits)
+    bits.append(1)
+        
+    return ''.join(str(bit) for bit in bits)
 
 text = input("Choose something: ")
 decimal_text = text_to_decimals(text)
 print(f"Decimals: {decimal_text}")
 
-encode = []
+encodes = []
 
+# join every encode bit
 for decimal in decimal_text:
     bits = fibonacci_encode(decimal)
-    encode.append(bits)  
+    encodes.append(bits)  
     
+final_encode = ''.join(str(encode) for encode in encodes)
 
-print(f"Encode -> {encode}")
+print(f"Encode -> {final_encode}")
